54b80437a74d0ec4995f8e2b3e9e79739f1dd97a
JUMARY <36880881+JUMARY@users.noreply.github.com>
2018-08-20 12:01:42 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+int num;
+printf("enter a number")
+scanf("%d",num);{
+if(num<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("the number is positive");
+}
+}
+getch();}
